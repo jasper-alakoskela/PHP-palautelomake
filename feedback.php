ae1aeca62adeb087edadaba4ce55e41c945c7037
Jasper Alakoskela <jasper.alakoskela@gmail.com>
2021-03-29 13:28:26 +0300

    palaute nettisivulle melkein valmis

---
+++ feedback.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Palaute</title>
+</head>
+
+<body>
+    <h1>Palaute</h1>
+    <br><hr>
+    <?php
+    if (isset($_POST["submit"])) {
+
+        if (empty($_POST["message"])) {
+            echo "<script>location.href='index.php'</script>";
+            return  false;
+        }
+
+        // Muokataan objekti
+        $xml = simplexml_load_file("data.xml");
+        $form = [$_POST["name"], $_POST["email"], $_POST["message"], $_POST["rate"]];
+        $newGuest = $xml -> addChild("feedback");
+        $newGuest -> addChild("name", $form[0]);
+        $newGuest -> addChild("email", $form[1]);
+        $newGuest -> addChild("message", $form[2]);
+        $newGuest -> addChild("rate", $form[3]);
+
+        // Tallennetaan muokattu objekti tiedostoon
+        $dom = new DOMDocument("1.0");
+        $dom -> preserveWhiteSpace = false;
+        $dom -> formatOutput = true;
+        $dom -> loadXML($xml->asXML());
+        $dom -> save("data.xml");
+
+        // Näytetään XML objekti sivulla
+        $xml = simplexml_load_file("data.xml");
+        foreach ($xml->feedback as $feedback) {
+        echo "<p>$feedback->name</p>";
+        echo "<p>$feedback->email<p>";
+        echo "<p>$feedback->message<p>";
+        echo "<img src='$feedback->rate'>";
+        echo "<hr>";
+        }
+    }   
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Palautelomake</title>
 </head>
 
 <body>
     <h1>Täytä lomake</h1>
-    <form action="" method="">
+    <form action="feedback.php" method="POST">
         <label for="name">Nimi</label>
         <br><br>
         <input type="text" name="name">
@@ -22,19 +22,22 @@
         <br><br>
         <label for="message">Palaute</label>
         <br><br>
-        <textarea name="message" cols="30" rows="5"></textarea>
-        <br><br>
-
+        <textarea name="message" cols="30" rows="5" placeholder="pakollinen"></textarea>
+        <br>
+        <?php
+            if (empty($_POST["message"])) {
+                echo "<p style='color:maroon;'>Kirjoita palaute!</p>";
+            }?>
+        
         <label for="like">
-            <input type="radio" id="like" name="rate" value="like">
+            <input type="radio" id="like" name="rate" value="img/thumb_up.png">
             <img src="img/thumb_up.png">
         </label>
 
         <label for="dislike">
-            <input type="radio" id="dislike" name="rate" value="dislike">
+            <input type="radio" id="dislike" name="rate" value="img/thumb_down.png">
             <img src="img/thumb_down.png">
         </label>
-
         <br><br>
         <input type="submit" name="submit">
     </form>
